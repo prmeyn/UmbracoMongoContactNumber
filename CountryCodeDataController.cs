9a9738690f5e15bca8b3fc18d373e9f476e812bb
Prashansa Meyn <pm@globalpay.dk>
2022-09-24 22:07:52 +0200

    why this again

--- CountryCodeDataController.cs
+++ CountryCodeDataController.cs
@@ -13,7 +13,7 @@ namespace UmbracoMongoContactNumber
 	public class CountryCodeDataApiController : UmbracoAuthorizedJsonController
 	{
 		[HttpGet]
-		public string GetCountryPhoneCodeDataDictionary()
+		public static string GetCountryPhoneCodeDataDictionary()
 		{
 			return JsonConvert.SerializeObject(SmsDataService.GetAllCountryPhoneCodesDictionary);
 		}
