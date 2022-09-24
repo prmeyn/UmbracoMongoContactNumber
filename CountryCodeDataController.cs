972d476520abe2bcf8336bf7d960aba6de0e88b8
Prashansa Meyn <pm@globalpay.dk>
2022-09-24 22:25:35 +0200

    static is 404

--- CountryCodeDataController.cs
+++ CountryCodeDataController.cs
@@ -13,7 +13,7 @@ namespace UmbracoMongoContactNumber
 	public class CountryCodeDataApiController : UmbracoAuthorizedJsonController
 	{
 		[HttpGet]
-		public static string GetCountryPhoneCodeDataDictionary()
+		public string GetCountryPhoneCodeDataDictionary()
 		{
 			return JsonConvert.SerializeObject(SmsDataService.GetAllCountryPhoneCodesDictionary);
 		}
